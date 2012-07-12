1056bf22a4a0ee5b85363b89f6cb49947b3e826d
brycesenz <bryce.senz@gmail.com>
2012-07-12 09:19:12 +0700

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Traillove::Application.config.secret_token = '90dd6d7c5581c2a9c444476be2df7e54bf3770f729b5bd5d841a25b0f09b3759fe5684cb16b3d18ba46121092b24916a92d0bddb762fc76a9ebfc3c572b1616e'
